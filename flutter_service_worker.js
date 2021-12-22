a1287126f32d2e0e8c1e9f35df10f8ac7470f096
ramceo <rameojack@icloud.com>
2021-12-23 01:16:45 +0530

    final correction

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "eb3562e2639f9c75ef33827f6c450a3f",
-"index.html": "1cc562329974fd7db69723167108239e",
-"/": "1cc562329974fd7db69723167108239e",
-"main.dart.js": "07ab734281b411aa15cebed430ce7157",
+"index.html": "7c1e4bdecc080f485425dd3b27637fe3",
+"/": "7c1e4bdecc080f485425dd3b27637fe3",
+"main.dart.js": "5bf4c2333ab5108118dea36adecfcca7",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
